234201c6abffe3f6d089f2746fbeb54db78fbbe6
AvtoVAZ <91632854+AvtoVAZ@users.noreply.github.com>
2023-04-10 22:11:55 +0300

    Add files via upload

---
+++ diakrinousa_solution_step_by_step.py
@@ -0,0 +1,18 @@
+alpha_srt = input("What is the value of alpha: ")
+alpha = int(alpha_srt)
+beta_str = input("Whats is the value of beta: ")
+beta = int(beta_str)
+gamma_str = input("What is the value of gamma: ")
+gamma = int(gamma_str)
+
+diakrinousa1 = ((beta) * (beta))
+diakrinousa2 = ((alpha * gamma))
+diakrinousa3 = (4 * (diakrinousa2))
+diakrinousa4 = ((diakrinousa1) - (diakrinousa3))
+
+print("Δ = (",(diakrinousa1),") - 4 * (",(diakrinousa2),")")
+print("Δ = (",(diakrinousa1),") - (",diakrinousa3,")")
+print("Δ = (",diakrinousa4,")")
+
+
+
